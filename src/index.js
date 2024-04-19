b88ce7deddfedc4cb5f4ff749e9e1bb7df1627e5
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-19 16:07:23 -0400

    connected react app to redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
 
 import store from "./store";
 store.dispatch({ type: "account/deposit", payload: 250 });
@@ -9,6 +10,8 @@ console.log(store.getState());
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
