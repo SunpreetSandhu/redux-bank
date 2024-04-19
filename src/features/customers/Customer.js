26eed2cf16c677fad87cee8c1899da476d94887b
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-19 16:47:53 -0400

    conditionally rendered create account page

--- src/features/customers/Customer.js
+++ src/features/customers/Customer.js
@@ -1,8 +1,14 @@
 import { useSelector } from "react-redux";
 function Customer() {
   const customer = useSelector((store) => store.customer.fullName);
+  const nationalId = useSelector((store) => store.customer.nationalId);
+
   console.log(customer);
-  return <h2>👋 Welcome, {customer}</h2>;
+  return (
+    <h2>
+      👋 Welcome, {customer} with national ID: {nationalId}
+    </h2>
+  );
 }
 
 export default Customer;
