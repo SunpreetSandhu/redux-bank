8229b988cacaed69f74e74c226fc452f419a50a9
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-04-21 15:58:50 -0400

    made reqloan acceptt 2 args

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -16,13 +16,22 @@ const accountSlice = createSlice({
     withdraw(state, action) {
       state.balance -= action.payload;
     },
-    requestLoan(state, action) {
-      if (state.loan > 0) {
-        return;
-      }
-      state.loan = action.payload.amount;
-      state.loanPurpose = action.payload.purpose;
-      state.balance = state.balance + action.payload.amount;
+
+    //more than 1 arg
+    requestLoan: {
+      prepare(amount, purpose) {
+        return {
+          payload: { amount, purpose },
+        };
+      },
+      reducer(state, action) {
+        if (state.loan > 0) {
+          return;
+        }
+        state.loan = action.payload.amount;
+        state.loanPurpose = action.payload.purpose;
+        state.balance = state.balance + action.payload.amount;
+      },
     },
     payLoan(state, action) {
       state.loan = 0;
